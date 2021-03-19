5301e4af6c1e36876a189bf484f16efe20e5e50c
Will <williamherbert.trevis@boeing.com>
2021-03-19 19:31:42 +0000

    materials ready for next session -WT

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function openEmailClient(){
     window.open('mailto:sibsabu@gmail.com')
 }
+
+function toggleMode() {
+    let mode = getComputedStyle(document.documentElement)
+        .getPropertyValue('--bg-color');
+    if(mode === 'whitesmoke') {
+        document.documentElement.style.setProperty('--bg-color', 'black');
+        document.documentElement.style.setProperty('--text-color', 'white');
+    } else {
+        document.documentElement.style.setProperty('--bg-color', 'whitesmoke');
+        document.documentElement.style.setProperty('--text-color', 'black');
+    }
+}
