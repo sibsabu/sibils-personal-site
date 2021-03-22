462bce628c13a2a338ef937f13848992218c8eb4
Will <williamherbert.trevis@boeing.com>
2021-03-22 19:39:59 +0000

    configuring smooth scrolling -WT

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function openEmailClient(){
 function toggleMode() {
     let mode = getComputedStyle(document.documentElement)
         .getPropertyValue('--bg-color');
+    console.log(mode);
     if(mode === 'whitesmoke') {
         document.documentElement.style.setProperty('--bg-color', 'black');
         document.documentElement.style.setProperty('--text-color', 'white');
@@ -13,3 +14,19 @@ function toggleMode() {
         document.documentElement.style.setProperty('--text-color', 'black');
     }
 }
+
+window.onload=function(){
+// listen for clicks on the navbar
+    document.querySelector('.navbar').addEventListener('click', (e) => {
+
+        // ignore it if the click isn't on an anchor element
+        if (e.target.tagName.toLowerCase() === 'a') {
+
+            // remove the 'active' class from all of the nav anchors
+            document.querySelectorAll('.navbar a')
+                .forEach(e => e.classList.remove('active'));
+
+            // add the 'active' class to the clicked element
+            e.target.classList.add('active');
+        }
+    });}
