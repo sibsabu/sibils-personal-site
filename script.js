770bc6df8de610a5ed7bdc994e800fd9de7c336c
sibsabu <sibsabu@gmail.com>
2021-03-24 10:30:03 +0000

    we have some errors to fix now -ss

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ function openEmailClient() {
     window.open('mailto:sibsabu@gmail.com');
 }
 
-function toggleNightMode() {
-    let mode = getComputedStyle(document.documentElement)
-        .getPropertyValue('--bg-colour');
-    console.log(mode);
+window.onload=function(){
+    document.querySelector('#toggle-night-mode').addEventListener("click", function () {
+        let mode = getComputedStyle(document.documentElement)
+            .getPropertyValue('--bg-colour');
+        console.log(mode);
 
-    if(mode === 'whitesmoke') {
-        document.documentElement.style.setProperty('--bg-colour', 'black');
-        document.documentElement.style.setProperty('--text-colour', 'white');
-    } else {
-        document.documentElement.style.setProperty('--bg-colour', 'whitesmoke');
-        document.documentElement.style.setProperty('--text-colour', 'black');
-    }
+        if (mode !== 'black') {
+            document.documentElement.style.setProperty('--bg-colour', 'black');
+            document.documentElement.style.setProperty('--text-colour', 'white');
+        } else {
+            document.documentElement.style.setProperty('--bg-colour', 'whitesmoke');
+            document.documentElement.style.setProperty('--text-colour', 'black');
+        }
+    });
 }
